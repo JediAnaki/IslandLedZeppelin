a90e965846dd0a5a6b7f5545c7b568066772d380
JediAnaki <elspiderman2307@gmail.com>
2025-10-23 23:08:59 +0400

    start project

---
+++ src/main/java/com/javarush/island/kulikov/ConsoleRunner.java
@@ -0,0 +1,9 @@
+package com.javarush.island.kulikov;
+
+public class ConsoleRunner {
+
+    public static void main(String[] args) {
+            
+    }
+
+}
---
+++ src/main/java/com/javarush/island/kulikov/entity/organizm/animals/Animal.java
@@ -0,0 +1,22 @@
+package com.javarush.island.kulikov.entity.organizm.animals;
+
+import com.javarush.island.kulikov.entity.map.Cell;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public abstract class Animal {
+
+    private Double weight;
+    private Integer speed;
+    private Integer foodNeeded;
+    private Integer currentFood;
+    private Cell currentCell;
+
+    public abstract void move();
+    public abstract void eat();
+    public abstract void reproduce();
+    public abstract boolean isDead();
+
+}
